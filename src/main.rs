584460bc7f97f37c79beb5d88829e96c2ccaea71
ili <blackey2309@gmail.com>
2023-03-27 00:38:19 +0900

    blocks in field

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+enum BlockKind {
+    I,
+    O,
+    S,
+    Z,
+    J,
+    L,
+    T,
+}
+
+type BlockShape = [[usize; 4]; 4];
+const BLOCKS: [BlockShape; 7] = [
+    // I block
+    [
+        [0,0,0,0],
+        [0,0,0,0],
+        [1,1,1,1],
+        [0,0,0,0],
+    ],
+    // O block
+    [
+        [0,0,0,0],
+        [0,1,1,0],
+        [0,1,1,0],
+        [0,0,0,0],
+    ],
+    //S block
+    [
+        [0,0,0,0],
+        [0,1,1,0],
+        [1,1,0,0],
+        [0,0,0,0],
+    ],
+    // Z block
+    [
+        [0,0,0,0],
+        [1,1,0,0],
+        [0,1,1,0],
+        [0,0,0,0],
+    ],
+    // J block
+    [
+        [0,0,0,0],
+        [1,0,0,0],
+        [1,1,1,0],
+        [0,0,0,0],
+    ],
+    // L block
+    [
+        [0,0,0,0],
+        [0,0,1,0],
+        [1,1,1,0],
+        [0,0,0,0],
+    ],
+    // T block
+    [
+        [0,0,0,0],
+        [0,1,0,0],
+        [1,1,1,0],
+        [0,0,0,0],
+    ],
+];
+
 fn main() {
     let field = [
         [1,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -23,10 +86,25 @@ fn main() {
         [1,1,1,1,1,1,1,1,1,1,1,1,1],
     ];
 
+    // 描画用フィールドの生成
+    let mut field_buf = field;
+    // 描画用フィールドにブロックの情報を書き込む
+    for y in 0..4 {
+        for x in 0..4 {
+            field_buf[y+2][x+2] = BLOCKS[BlockKind::I as usize][y][x];
+            field_buf[y+2][x+7] = BLOCKS[BlockKind::O as usize][y][x];
+            field_buf[y+6][x+2] = BLOCKS[BlockKind::S as usize][y][x];
+            field_buf[y+6][x+7] = BLOCKS[BlockKind::Z as usize][y][x];
+            field_buf[y+10][x+2] = BLOCKS[BlockKind::J as usize][y][x];
+            field_buf[y+10][x+7] = BLOCKS[BlockKind::L as usize][y][x];
+            field_buf[y+14][x+2] = BLOCKS[BlockKind::T as usize][y][x];
+        }
+    }
+
     // フィールドを描画
     for y in 0..21 {
         for x in 0..13 {
-            if field[y][x] == 1 {
+            if field_buf[y][x] == 1 {
                 print!("[]");
             } else {
                 print!(" .");
